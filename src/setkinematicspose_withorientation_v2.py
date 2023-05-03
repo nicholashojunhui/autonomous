36120045a6331e7998c2bcb0b956769cac1e5109
nicholashojunhui <37932759+nicholashojunhui@users.noreply.github.com>
2023-05-03 08:53:25 +0800

    Add files via upload

---
+++ src/setkinematicspose_withorientation_v2.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+# works for actual OM ONLY!
+
+import rospy					#import the python library for ROS
+from open_manipulator_msgs.msg import JointPosition	#import JointPosition message from the open_manipulator_msgs package
+from open_manipulator_msgs.srv import SetJointPosition
+from open_manipulator_msgs.msg import KinematicsPose
+from open_manipulator_msgs.srv import SetKinematicsPose
+from geometry_msgs.msg import Pose
+import math
+
+def talker():
+	rospy.init_node('OM_publisher')	#Initiate a Node called 'OM_publisher'
+	set_kinematics_position = rospy.ServiceProxy('/goal_joint_space_path_to_kinematics_pose', SetKinematicsPose)
+	set_gripper_position = rospy.ServiceProxy('/goal_tool_control', SetJointPosition)
+	
+	#while not rospy.is_shutdown():
+	#move front
+	kinematics_pose = KinematicsPose()
+	kinematics_pose.pose.position.x = 0.2867
+	kinematics_pose.pose.position.y = 0.0
+	kinematics_pose.pose.position.z = 0.194
+
+	kinematics_pose.pose.orientation.x = 1.765
+	kinematics_pose.pose.orientation.y = 0.023
+	kinematics_pose.pose.orientation.z = 0.0
+	kinematics_pose.pose.orientation.w = 0.999
+
+	resp1 = set_kinematics_position('planning_group', 'gripper', kinematics_pose, 3)
+	
+	rospy.sleep(3)
+	#while not rospy.is_shutdown():
+	
+	#Gripper bend down (at front side)
+	kinematics_pose = KinematicsPose()
+	kinematics_pose.pose.position.x = 0.276
+	kinematics_pose.pose.position.y = 0.000
+	kinematics_pose.pose.position.z = 0.155
+
+	kinematics_pose.pose.orientation.x = 0.000
+	kinematics_pose.pose.orientation.y = 0.199
+	kinematics_pose.pose.orientation.z = 0.000
+	kinematics_pose.pose.orientation.w = 0.98
+
+	resp1 = set_kinematics_position('planning_group', 'gripper', kinematics_pose, 3)
+
+	gripper_position = JointPosition()
+	gripper_position.joint_name = ['gripper']
+	gripper_position.position =  [0.01]	# -0.01 for fully close and 0.01 for fully open
+	respg2 = set_gripper_position('planning_group',gripper_position, 3)
+
+	rospy.sleep(3)
+	#move left
+	kinematics_pose = KinematicsPose()
+	kinematics_pose.pose.position.x = 0.0128
+	kinematics_pose.pose.position.y = 0.2748
+	kinematics_pose.pose.position.z = 0.1938
+
+	kinematics_pose.pose.orientation.w = 0.707
+	kinematics_pose.pose.orientation.x = -0.016
+	kinematics_pose.pose.orientation.y = 0.016
+	kinematics_pose.pose.orientation.z = 0.7058
+
+	resp1 = set_kinematics_position('planning_group', 'gripper', kinematics_pose, 3)
+	
+	rospy.sleep(3)
+
+	#Gripper bend down (at left side)
+	kinematics_pose = KinematicsPose()
+	kinematics_pose.pose.position.x = 0.012
+	kinematics_pose.pose.position.y = 0.268
+	kinematics_pose.pose.position.z = 0.167
+
+	kinematics_pose.pose.orientation.x = -0.105
+	kinematics_pose.pose.orientation.y = 0.106
+	kinematics_pose.pose.orientation.z = 0.699
+	kinematics_pose.pose.orientation.w = 0.699
+
+	resp1 = set_kinematics_position('planning_group', 'gripper', kinematics_pose, 3)
+	
+	rospy.sleep(3)
+
+
+	gripper_position = JointPosition()
+	gripper_position.joint_name = ['gripper']
+	gripper_position.position =  [-0.01]	# -0.01 for fully close and 0.01 for fully open
+	respg2 = set_gripper_position('planning_group',gripper_position, 3)
+	
+
+if __name__== '__main__':
+	try:
+		talker()
+	except rospy.ROSInterruptException:
+		pass
